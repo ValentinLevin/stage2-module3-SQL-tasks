707fe74da427ca134f53a664f1eba5f36eef378e
Valentin Levin <levin_valentin@mail.ru>
2024-01-26 10:11:06 +0600

    solution #19

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
- delete from student where id in (select id from student where groupnumber > 4 limit 0);
+ delete from student where groupnumber > 4;
  delete from student where id in (select distinct student_id from mark where mark < 4);
  delete from paymenttype where name = 'DAILY';
- delete from mark where id in (select id from mark where mark <= 8 limit 0);
+ delete from mark where mark < 7;
